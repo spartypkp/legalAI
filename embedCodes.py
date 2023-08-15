44a92e90bd4f732654d81b8e269f2f096a838e98
Will Diamond <59883254+spartypkp@users.noreply.github.com>
2023-08-15 16:32:34 -0700

    Refactored: Needs 1 input file and logs errors

--- embedCodes.py
+++ embedCodes.py
@@ -2,30 +2,39 @@ import openai
 from tenacity import retry, wait_random_exponential, stop_after_attempt
 import json
 import config
-
+import os
 
 # legalMap Secret API Key
 openai.api_key = config.spartypkp_openai_key
 codes = ["BPC","CCP","CIV","COM","CONS","CORP","EDC","ELEC","EVID","FAC","FAM","FGC","FIN","GOV","HNC","HSC","INS","LAB","MVC","PCC","PEN","PRC","PROB","PUC","RTC","SHC","UIC","VEH","WAT","WIC"]
+DIR = os.path.dirname(os.path.realpath(__file__))
 
 # Left blank for testing
 def main():
-    pass
+    for code in codes:
+        print("Started embedding codes for code {}...".format(code))
+        embed_Codes(code)
+        print("Finished embedding for code {}".format(code))
 
-# Deprecated function
-def embed_Codes(textDct):
-    embedded={}
-    code = None
+# TODO: Write fn description
+def embed_Codes(code):
+    with open("{}/scrapedData/{}.txt", "r") as text_file:
+        rawText = text_file.read()
+        textDct = json.loads(rawText)
+    text_file.close()
+
+    file = open("embeddingErrorLog.txt", "a")
     for key, value in textDct.items():
-        splt = value.split(",")
-        numTokens = int(splt[0])
-        if numTokens > 7000:
-            continue
-        embedding = get_embedding(value)
-        print("Embedded for key {}".format(key))
-        embedded[key] = embedding
-    with open("/codeEmbeds/{}embed.txt".format(code), "w") as out_file:
-        out_file.write(json.dumps(embedded))
+        embed = []
+        try:
+            embed = get_embedding(value[9])
+        except Exception as e:
+            
+            file.write("({})\n".format(key))
+    file.close()
+        
+    with open("{}/scrapedData/{}.txt", "w") as out_file:
+        out_file.write(json.dumps(textDct))
     out_file.close()
 
 # Need tweaking on the min and max parameters.
