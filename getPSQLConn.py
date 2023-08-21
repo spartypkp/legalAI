2653e09094ef6002982bbdc36f31c474e5b2e6c3
Will Diamond <59883254+spartypkp@users.noreply.github.com>
2023-08-21 14:31:59 -0700

    Refactor: add execute_sql_select method

--- getPSQLConn.py
+++ getPSQLConn.py
@@ -17,6 +17,12 @@ def connect():
         print(error)
         raise error
 
+def select_and_fetch_rows(conn, sql_select):
+    cursor = conn.cursor()
+    cursor.execute(sql_select)
+    rows = cursor.fetchall()
+    cursor.close()
+    return rows
 
 if __name__ == '__main__':
     connect()
