4a1e0ee351628cea7162420b5424327d54fac2af
Will Diamond <59883254+spartypkp@users.noreply.github.com>
2023-08-27 15:47:47 -0700

    Moved prompts to prompt storage

--- createAbe.py
+++ createAbe.py
@@ -3,6 +3,7 @@ import openai
 import config
 import json
 import compareEmedding
+import promptStorage as prompts
 
 openai.api_key = config.spartypkp_openai_key
 
@@ -24,96 +25,60 @@ def main():
     
     
     
-def ask_abe(user_question):  
-    user_question = input("What question would you like abe to answer?\n")
-    topic_dict = get_similar_topics(user_question)
+def ask_abe(user_query):  
+    user_query = input("What question would you like abe to answer?\n")
+    topic_dict = get_similar_queries(user_query)
     topics_str = " ".join(topic_dict["queries"])
     print("\n\n Calling GPT 3.5 to generate related questions...: \n", topics_str)
     print("\n Comparing vector embeddings in the database to embedding of all related quries....\n")
-    rows = compareEmedding.compare_all_embeddings(topics_str, match_count=20)
+    rows = compareEmedding.compare_all_embeddings(topics_str, print_relevant_sections=False, match_count=20)
     formatted_section = compareEmedding.format_sql_rows(rows)
     #continue_to_answer = input("These are all the relevant sections above. Would you like to continue to get a full answer from GPT? (y/n)")
     continue_to_answer = "y"
     if continue_to_answer == "y":
-        final_answer = get_final_answer(user_question, rows)
+        print("Using gpt-3.5-turbo, 16k token limit")
+        final_answer = get_final_answer(user_query, rows)
+        print("\n\n")
+        print(final_answer)
+        print("Using gpt-4-32k, sorry Sean it's expensive")
+        final_answer = get_final_answer(user_query, rows, use_gpt_4=True)
         print("\n\n")
         print(final_answer)
         return final_answer, formatted_section
 
-def get_similar_topics(user_question):
-    similar_topics= [
-        {"role": "system", "content": "All output shall be in JSON format."},
-        {"role": "user", "content": '''Generate an array of search queries that are relevant to this question.
-
-Use a variation of related keywords for the queries, trying to be as general as possible.
-
-Queries should include legal language such as ["lawful", "violation", "authorized", "restrictions", "legitimate", "defined", "according to law", "legal"].
-
-Queries should be short.
-
-Generate 15 queries.
-
-User question: {}
-            
-Format: {{\\"queries\\": [\\"query_1\\", \\"query_2\\", \\"query_3\\"]}}";'''.format(user_question)}]
-    
-    chat_completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=similar_topics)
+def get_similar_queries(user_query):
+    similar_topics= prompts.get_prompt_similar_queries(user_query)
+    chat_completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=similar_topics, temperature=0.4)
     result = chat_completion.choices[0].message.content
     result_dct = json.loads(result)
     return result_dct
 
-def extract_questions_from_text():
-    prompt1 = '''You are an editor for a law firm that helps explain legal text. 
-
-    You will be given a section of legal code delimitted by triple quotes. 
-
-    Make a list of questions that could be answered by this section of legal code. Questions you make will be used to link a frequently asked section page to this document. 
-
-    Be creative and create as many question as you can from every part of the text.
-
-    '''
-    prompt2 = '''You are a helpful and thorough teachers assistant in a law school. You focus on assisting professors by creating rigorous questions that test's a student understanding of a piece of legislation.
-
-    Generate questions that this piece of legislation answers.
-
-    Read carefully and take your time. Create a list of 20 questions.
-        '''
+def generate_hypothetical_questions():
+    prompt = prompts.get_prompt_generate_hypothetical_questions()
+    # Call openai.ChatCompletion.create(model, messages=, temperature=)
     return
 
-def get_final_answer(user_question, rows):
+def get_final_answer(user_query, rows, use_gpt_4=False):
     current_tokens = 0
     row = 0
-    text = ""
-    max_tokens = 12000
+    legal_text = ""
+    if use_gpt_4:
+        max_tokens = 24000
+    else:
+        max_tokens = 12000
     while current_tokens < max_tokens and row < len(rows):
-
         #print(rows[row])
         current_tokens += rows[row][10]
-        text = text + "\n" + rows[row][9]
+        legal_text = legal_text + "\n" + rows[row][9]
         row += 1
     
-    answer_prompt= [
-        {"role": "system", "content": '''You are a helpful legal assistant that answers a user query by summarizing information in a legal document.
+    answer_prompt= prompts.get_prompt_final_answer(user_query, legal_text)
 
-        You will be provided with a user query and legal documentation in the format of a dictionary. 
-
-        All provided legal documentation is verified to be up to date, legally accurate, and not subject to change.'''},
-        {"role": "user", "content":''' Carefully read the entire legal documentation and answer the following from the documentation:
-1. After reading the entire document, what is the simple answer to the user's query?
-2. What exact text of the legal documentation shows the simple answer to the user's query?
-3. What rights and privileges does a user have relating to their query?
-4. What are restrictions, caveats, and conditions to the user's query?
-5. What are any penalties, punishments, or crimes which apply to violating restrictions of the user's query?
-
-For every question you answer with information from the legal documentation, annotate the answer with a citation using the format:
-Question answer. (Section). 
-
-If a question isn't related to the user's query, do not answer it.
-         
-The more detail you include in your answers, the more you help the user. Include all relevant information in each answer.
-
-[User query: {}, Legal documentation:{}]'''.format(user_question, text)}]
-    chat_completion = openai.ChatCompletion.create(model="gpt-3.5-turbo-16k",messages=answer_prompt)
+    if use_gpt_4:
+        openai.api_key = config.spartypkp_openai_key
+        chat_completion = openai.ChatCompletion.create(model="gpt-4-32k",messages=answer_prompt, temperature=0.2)
+    else:
+        chat_completion = openai.ChatCompletion.create(model="gpt-3.5-turbo-16k",messages=answer_prompt, temperature=0.2)
     result = chat_completion.choices[0].message.content
     return result
 
