aaa0f224cfd10ff3336a54865359a2a016f35f8d
Will Diamond <59883254+spartypkp@users.noreply.github.com>
2023-08-27 17:03:54 -0700

    Refactor: Can be called without main

--- createAbe.py
+++ createAbe.py
@@ -21,34 +21,40 @@ openai.api_key = config.spartypkp_openai_key
 
 
 def main():
-    ask_abe("Placeholder")
-    
+    user_query = input("What question would you like abe to answer?\n")
+    ask_abe(user_query)
     
     
 def ask_abe(user_query):  
-    user_query = input("What question would you like abe to answer?\n")
+    # Get similar queries by calling GPT 3.5, maybe Google BARD instead
     topic_dict = get_similar_queries(user_query)
     topics_str = " ".join(topic_dict["queries"])
+    # Debug prints, print related queries
     print("\n\n Calling GPT 3.5 to generate related questions...: \n", topics_str)
     print("\n Comparing vector embeddings in the database to embedding of all related quries....\n")
+    # Get cosine similarity score of related queries to all content embeddings
     rows = compareEmedding.compare_all_embeddings(topics_str, print_relevant_sections=False, match_count=20)
     formatted_section = compareEmedding.format_sql_rows(rows)
-    #continue_to_answer = input("These are all the relevant sections above. Would you like to continue to get a full answer from GPT? (y/n)")
+
+    # continue to answer = input("Would you like to continue to GPT 4's answer? (y/n):\n")
+    
     continue_to_answer = "y"
     if continue_to_answer == "y":
         print("Using gpt-3.5-turbo, 16k token limit")
         final_answer = get_final_answer(user_query, rows)
         print("\n\n")
         print(final_answer)
+        '''
         print("Using gpt-4-32k, sorry Sean it's expensive")
         final_answer = get_final_answer(user_query, rows, use_gpt_4=True)
         print("\n\n")
         print(final_answer)
+        '''
         return final_answer, formatted_section
 
 def get_similar_queries(user_query):
-    similar_topics= prompts.get_prompt_similar_queries(user_query)
-    chat_completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=similar_topics, temperature=0.4)
+    prompt_similar_queries = prompts.get_prompt_similar_queries(user_query)
+    chat_completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=prompt_similar_queries, temperature=0.4)
     result = chat_completion.choices[0].message.content
     result_dct = json.loads(result)
     return result_dct
@@ -72,13 +78,13 @@ def get_final_answer(user_query, rows, use_gpt_4=False):
         legal_text = legal_text + "\n" + rows[row][9]
         row += 1
     
-    answer_prompt= prompts.get_prompt_final_answer(user_query, legal_text)
-
+    prompt_final_answer= prompts.get_prompt_final_answer(user_query, legal_text)
+    
     if use_gpt_4:
-        openai.api_key = config.spartypkp_openai_key
-        chat_completion = openai.ChatCompletion.create(model="gpt-4-32k",messages=answer_prompt, temperature=0.2)
+        openai.api_key = config.seangrove_openai_key
+        chat_completion = openai.ChatCompletion.create(model="gpt-4-32k",messages=prompt_final_answer, temperature=0.2)
     else:
-        chat_completion = openai.ChatCompletion.create(model="gpt-3.5-turbo-16k",messages=answer_prompt, temperature=0.2)
+        chat_completion = openai.ChatCompletion.create(model="gpt-3.5-turbo-16k",messages=prompt_final_answer, temperature=0.2)
     result = chat_completion.choices[0].message.content
     return result
 
