e267082a54f9a7612184047567ace6d7191071f4
Will Diamond <59883254+spartypkp@users.noreply.github.com>
2023-08-28 15:12:59 -0700

    Working version

--- createAbe.py
+++ createAbe.py
@@ -13,12 +13,21 @@ openai.api_key = config.spartypkp_openai_key
 # Input Cost: $0.003 per 1k
 # Output Cost: $0.004 per 1k
 
+
 # gpt-3.5-turbo-4k
 # Tokens Per Minute: 80k
 # Requests Per Minute: 3,500
 # Input Cost: $0.0015 per 1k
 # Output Cost: $0.002 per 1k
 
+# gpt-4 (8k token limit)
+# 
+# $0.03/1k prompt tokens
+# $0.06/1k sampled tokens
+
+# gpt-4-32k
+# $0.06/1k prompt tokens
+# $0.12/1k sampled tokens
 
 def main():
     user_query = input("What question would you like abe to answer?\n")
@@ -33,7 +42,7 @@ def ask_abe(user_query):
     print("\n\n Calling GPT 3.5 to generate related questions...: \n", topics_str)
     print("\n Comparing vector embeddings in the database to embedding of all related quries....\n")
     # Get cosine similarity score of related queries to all content embeddings
-    rows = compareEmedding.compare_all_embeddings(topics_str, print_relevant_sections=False, match_count=20)
+    rows = compareEmedding.compare_all_embeddings(user_query, print_relevant_sections=True, match_count=20)
     formatted_section = compareEmedding.format_sql_rows(rows)
 
     # continue to answer = input("Would you like to continue to GPT 4's answer? (y/n):\n")
@@ -64,10 +73,20 @@ def generate_hypothetical_questions():
     # Call openai.ChatCompletion.create(model, messages=, temperature=)
     return
 
+def calculate_prompt_cost(model, prompt_tokens, completion_tokens):
+    model_rates = {"gpt-3.5-turbo-16k":[0.003, 0.004], "gpt-3.5-turbo-4k":[0.0015, 0.002], "gpt-4":[0.03, 0.06], "gpt-4-32k":[0.06, 0.12]}
+    prompt_rate = model_rates[model][0]
+    completion_rate = model_rates[model][1]
+    cost = ((prompt_rate/1000)*prompt_tokens) + ((completion_rate/100)*completion_tokens)
+    print("Prompt Tokens: {}, Completion Tokens: {}".format(prompt_tokens, completion_tokens))
+    print("Total cost of using {}: ${}".format(model, cost))
+    return cost
+
 def get_final_answer(user_query, rows, use_gpt_4=False):
     current_tokens = 0
     row = 0
     legal_text = ""
+    used_model = "gpt-3.5-turbo-16k"
     if use_gpt_4:
         max_tokens = 24000
     else:
@@ -77,15 +96,23 @@ def get_final_answer(user_query, rows, use_gpt_4=False):
         current_tokens += rows[row][10]
         legal_text = legal_text + "\n" + rows[row][9]
         row += 1
-    
-    prompt_final_answer= prompts.get_prompt_final_answer(user_query, legal_text)
+    #print("Number of tokens in legal_text: ", current_tokens)
+    prompt_convert_question = prompts.get_prompt_convert_question(user_query)
+    chat_completion =  openai.ChatCompletion.create(model=used_model,messages=prompt_convert_question, temperature=0)
+    converted_questions = chat_completion.choices[0].message.content
+    template = prompts.get_basic_universal_answer_template(user_query, converted_questions)
+    prompt_final_answer= prompts.get_prompt_final_answer(user_query, legal_text, template)
     
     if use_gpt_4:
         openai.api_key = config.seangrove_openai_key
-        chat_completion = openai.ChatCompletion.create(model="gpt-4-32k",messages=prompt_final_answer, temperature=0.2)
+        used_model = "gpt-4-32k"
+        chat_completion = openai.ChatCompletion.create(model=used_model,messages=prompt_final_answer, temperature=0.2)
     else:
-        chat_completion = openai.ChatCompletion.create(model="gpt-3.5-turbo-16k",messages=prompt_final_answer, temperature=0.2)
+        chat_completion = openai.ChatCompletion.create(model=used_model,messages=prompt_final_answer, temperature=0.2)
     result = chat_completion.choices[0].message.content
+    prompt_tokens = chat_completion.usage["prompt_tokens"]
+    completion_tokens = chat_completion.usage["completion_tokens"]
+    cost = calculate_prompt_cost(used_model, prompt_tokens, completion_tokens)
     return result
 
 if __name__ == "__main__":
