3d22124198913cb56bc8b9305b5e7106990e72ea
Will Diamond <59883254+spartypkp@users.noreply.github.com>
2023-09-13 12:19:43 -0700

    moved all answering functionality here

--- answerUserQuery.py
+++ answerUserQuery.py
@@ -1,26 +1,50 @@
 import openai
 import utilityFunctions as util
 import config
+import promptStorage as prompts
 
 
 def main():
     pass
 
-def get_final_answer(prompt_final_answer, use_gpt_4):
-    used_model = "gpt-3.5-turbo-16k"
+def answering_stage(question_list, legal_text, use_gpt_4=True):
+    print("Starting answering stage...")
+    full_result, total_prompt_tokens, total_completion_tokens = answer_all_questions(question_list, use_gpt_4, legal_text)
+    return full_result, total_prompt_tokens, total_completion_tokens
+
+def answer_all_questions(question_list, use_gpt_4, legal_text):
+    full_result = ""
+    total_prompt_tokens = 0
+    total_completion_tokens = 0
+    
+    for i, question in enumerate(question_list):
+        if i == 0 or i == 1:
+            prompt_final_answer = prompts.get_prompt_simple_answer(legal_text, question)
+        else:
+            prompt_final_answer = prompts.get_prompt_final_answer(legal_text, question)
+        result, prompt_tokens, completion_tokens, cost = answer_one_question(prompt_final_answer, use_gpt_4)
+        
+        full_result += result
+        total_prompt_tokens += prompt_tokens
+        total_completion_tokens += completion_tokens
+        print(cost)
+    return full_result, total_prompt_tokens, total_completion_tokens
+
+
+def answer_one_question(prompt_final_answer, use_gpt_4):
+    model = "gpt-3.5-turbo-16k"
+    who = "will"
     if use_gpt_4:
-        openai.api_key = config.seangrove_openai_key
-        used_model = "gpt-4-32k"
-        chat_completion = openai.ChatCompletion.create(model=used_model,messages=prompt_final_answer, temperature=0.2)
-    else:
-        chat_completion = openai.ChatCompletion.create(model=used_model,messages=prompt_final_answer, temperature=0.2)
+        who = "sean"
+        model = "gpt-4-32k"
+
+    chat_completion = util.create_chat_completion(used_model=model, messages=prompt_final_answer, temp=0.2, api_key_choice=who)
+
     result_str = chat_completion.choices[0].message.content
-    result = result_str.split("*")
-    result = "\n".join(result[1:])
     prompt_tokens = chat_completion.usage["prompt_tokens"]
     completion_tokens = chat_completion.usage["completion_tokens"]
-    cost = util.calculate_prompt_cost(used_model, prompt_tokens, completion_tokens)
-    return result, prompt_tokens, completion_tokens, cost
+    cost = util.calculate_prompt_cost(model, prompt_tokens, completion_tokens)
+    return result_str, prompt_tokens, completion_tokens, cost
 
 if __name__ == "__main__":
     main()
